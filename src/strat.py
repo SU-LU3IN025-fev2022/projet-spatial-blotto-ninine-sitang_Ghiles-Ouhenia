454ad61ec89d2532cc79b4ba15902af74ce3f1e5
GhilesOuhenia <100412562+GhilesOuhenia@users.noreply.github.com>
2022-03-31 16:51:52 +0200

    Update strat.py

--- src/strat.py
+++ src/strat.py
@@ -111,17 +111,68 @@ def fictitous_play(goalStates,nbPlayers,liste_strat):
             gain_max = gain_i
             meilleurs_strat = meilleurs_strat_i
     return meilleurs_strat
+def unique(liste_strat):
+    strat = []
+    occ = []
+    for i in liste_strat :
+        if i in strat :
+            occ[strat.index(i)]+=1
+        else :
+            strat.append(i)
+            occ.append(1)
+    return strat,occ
+def fictitous_play(goalStates,nbPlayers,liste_strat,jour):
+    if(jour==0):
+        return alea(goalStates,nbPlayers)
+    strat , nb_occurence = unique(liste_strat)
+    
+    gain_max = 0
+    score_elec = [(0,0)] * len(goalStates)
+    for i in range(len(strat)):
+        for j in range(len(goalStates)):
+            score_elec[j] = (strat[i][j],0)
+        meilleurs_strat_i = perdant(goalStates, nbPlayers, score_elec , score_A = 1, score_B = 0)
+        if i==0 :
+            meilleurs_strat = meilleurs_strat_i
+        for j in range(len(goalStates)):
+            elec = goalStates.index(meilleurs_strat_i[j])
+            x,y = score_elec[elec]
+            y += 1
+            score_elec[elec]= (x,y)
+        gain_i = 0
+        for k in range(len(strat)):
+            score_A = 0
+            score_B = 0
+            for j in range(len(goalStates)):
+                x,y = score_elec[j]
+                score_elec[j] = (strat[k][j],y)
+            for v in range(len(score_elec)):
+                x,y = score_elec[v]        
+                if x>y:
+                    score_A +=1
+                elif y>x:
+                    score_B +=1
 
-    def stochastique_expert(goalstate,strategy,proba_stock):
-        p=np.random.randint(0,100)/100
-        p=p*sum(proba_stock) # car même si la somme des probabilités n'est pas égal à 1 on revient à la même chose en fesant p*sum(proba_stock)
-        cpt = 0
-        o1=[]
-        for i in range(len(proba_stock)):
-            cpt  += proba_stock[i]
-            if p < cpt:
-                for e in range(len(strategy[i])):
-                    for m in range(strategy[i][e]):
-                        o1.append(goalstate[e])
-                return o1
-        return
+            if score_A > score_B :
+                gain_i -= nb_occurence[k] #on pourrait aussi devisé pour avoir une proba (ça ne changera pas le résultat final )
+            elif score_A < score_B : #cas où meilleure réponse bat la stat_k
+                gain_i += nb_occurence[k]
+        if gain_i > gain_max:
+            gain_max = gain_i
+            meilleurs_strat = meilleurs_strat_i
+    
+    return meilleurs_strat
+def stochastique_expert(goalstate,strategy,proba_stock):
+    p=np.random.randint(0,100)/100
+    p=p*sum(proba_stock) # car même si la somme des probabilités n'est pas égal à 1 on revient à la même chose en fesant p*sum(proba_stock)
+    cpt = 0
+    o1=[]
+    for i in range(len(proba_stock)):
+        cpt  += proba_stock[i]
+        if p < cpt:
+            for e in range(len(strategy[i])):
+                for m in range(strategy[i][e]):
+                    o1.append(goalstate[e])
+            return o1
+    return
+
